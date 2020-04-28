742e8f2af36a6ca22fb8ee9eccc50f8ec5dd9cd0
mashkarharis <54017081+mashkarharis@users.noreply.github.com>
2020-04-28 16:52:45 +0530

    added -- signup form is still development

---
+++ WEBAPP/APP/globalfunction_and_settings.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+include_once 'site/Controller/SportController.php';
+include_once 'site/Controller/LoginController.php';
+include_once 'site/Controller/MemberController.php';
+include_once 'site/DB/DBData.php';
+include_once 'site/Model/sports.php';
+include_once 'site/Model/Login.php';
+?>
+
